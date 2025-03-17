67d4dad090d656edab5fb36114d50425a84e5a3d
MICHAEL <michaelcaceres633@gmail.com>
2025-03-16 22:01:42 -0500

    configurar host y puerto para render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import sqlite3
 from geopy.geocoders import Nominatim
 import requests
+import os
 
 app = Flask(__name__)
 geolocator = Nominatim(user_agent="ruta_optima")
@@ -102,4 +103,4 @@ def eliminar_parada(id):
     return jsonify({"status": "success"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
